de7b858eaf49520df3b7087ddc9fa2f48b735668
dattte <you@example.com>
2020-10-21 03:39:30 +0700

    datte

---
+++ lesson9/main.js
@@ -0,0 +1,13 @@
+var getSalery = (money) {
+    if (money < 15000000) {
+        console.log(15)
+    } else if (rate === 1) {
+        return 'D'
+    } else if (rate === 2) {
+        return 'C'
+    } else if (rate === 3) {
+        return 'B'
+    } else {
+        return 'A'
+    }
+}
